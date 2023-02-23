c2bf701058a58ff1d261c1cfb6c54c9ba784281e
lpokiuj <michael.sanjaya86@gmail.com>
2023-02-23 14:05:18 +0700

    Stored Procedure implementation

--- testt/Controllers/PromoController.cs
+++ testt/Controllers/PromoController.cs
@@ -3,11 +3,12 @@ using testt.Models;
 using Npgsql;
 using System.Text.Json.Nodes;
 using System.Data;
+using Newtonsoft.Json.Linq;
 
 namespace testt.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("promo")]
     public class PromoController : Controller
     {
         private readonly IConfiguration _configuration;
@@ -17,149 +18,144 @@ namespace testt.Controllers
             this._configuration = configuration;
         }
         [HttpGet]
-        public List<Promo> Index()
+        public JObject Index()
         {
-            List<Promo> promos = new List<Promo>();
-            string constr = _configuration.GetConnectionString("SampleDbConnection");
-            string query = "SELECT * FROM promos";
-            using (NpgsqlConnection con = new NpgsqlConnection(constr))
-            {
-                using (NpgsqlCommand cmd = new NpgsqlCommand(query))
-                {
-                    cmd.Connection = con;
-                    con.Open();
-                    using (NpgsqlDataReader sdr = cmd.ExecuteReader())
-                    {
-                        while(sdr.Read())
-                        {
-                            promos.Add(new Promo
-                            {
-                                Id = sdr.GetInt32(sdr.GetOrdinal("id")),
-                                PromoCode = sdr.GetString(sdr.GetOrdinal("promo_code")),
-                                PromoName = sdr.GetString(sdr.GetOrdinal("promo_name")),
-                                PromoDescription = sdr.GetString(sdr.GetOrdinal("promo_description")),
-                                PromoStart = sdr.GetDateTime(sdr.GetOrdinal("promo_start")),
-                                PromoEnd = sdr.GetDateTime(sdr.GetOrdinal("promo_end")),
-                            });
-                        }
-                    }
-                    con.Close();
-                }
-            }
-            if(promos.Count == 0 )
-            {
-                promos.Add(new Promo());
-            }
+            var returnMsg = new JObject();
 
-            return promos;
+            string constr = _configuration.GetConnectionString("SampleDbConnection");
+            string query = "get_all_promos";
+            var con = new NpgsqlConnection(constr);
+            var cmd = new NpgsqlCommand(query, con);
+     
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            var sdr = cmd.ExecuteReader();
+            var promos = new JArray();
+            while (sdr.Read())
+            {
+                var data = new JObject();
+                for (int i = 0; i < sdr.FieldCount; i++)
+                {
+                    data.Add(sdr.GetName(i), sdr.IsDBNull(i) ? null : JToken.FromObject(sdr[i]));
+                }
+                promos.Add(data);
+            }
+            con.Close();
+
+            returnMsg["statusCode"] = 200;
+            returnMsg["message"] = "Data retrieved successfully";
+            returnMsg["data"] = promos;
+            return returnMsg;
         }
 
         [HttpGet("{id}")]
-        public Promo findById(int id)
+        public JObject FindById(int id)
         {
-            Promo promo = new Promo();
-            string query = "SELECT * FROM promos WHERE id = @PromoId";
+            var returnMsg = new JObject();
+
             string constr = _configuration.GetConnectionString("SampleDbConnection");
-            using (NpgsqlConnection con = new NpgsqlConnection(constr))
+            string query = "get_promo_by_id";
+            var con = new NpgsqlConnection(constr);
+            var cmd = new NpgsqlCommand(query, con);
+
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("p_prm_id", id);
+            con.Open();
+            var sdr = cmd.ExecuteReader();
+            var promo = new JObject();
+            while (sdr.Read())
             {
-                using (NpgsqlCommand cmd = new NpgsqlCommand(query, con))
+                for (int i = 0; i < sdr.FieldCount; i++)
                 {
-                    cmd.Connection = con;
-                    cmd.Parameters.AddWithValue("PromoId", id);
-                    con.Open();
-                    using (NpgsqlDataReader sdr = cmd.ExecuteReader())
-                    {
-                        while (sdr.Read())
-                        {
-                            promo.Id = sdr.GetInt32(sdr.GetOrdinal("id"));
-                            promo.PromoCode = sdr.GetString(sdr.GetOrdinal("promo_code"));
-                            promo.PromoName = sdr.GetString(sdr.GetOrdinal("promo_name"));
-                            promo.PromoDescription = sdr.GetString(sdr.GetOrdinal("promo_description"));
-                            promo.PromoStart = sdr.GetDateTime(sdr.GetOrdinal("promo_start"));
-                            promo.PromoEnd = sdr.GetDateTime(sdr.GetOrdinal("promo_end"));
-                        }
-                    }
-                    con.Close();
+                    promo.Add(sdr.GetName(i), sdr.IsDBNull(i) ? null : JToken.FromObject(sdr[i]));
                 }
             }
+            con.Close();
 
-            return promo;
+            returnMsg["statusCode"] = 200;
+            returnMsg["message"] = "Data retrieved successfully";
+            returnMsg["data"] = promo;
+            return returnMsg;
         }
 
-        [HttpPost("insert_promo")]
-        public string insert([FromBody] JsonObject data)
+        [HttpPost]
+        public JObject Insert([FromBody] JObject data)
         {
-            string query = "INSERT INTO promos (promo_code, promo_name, promo_description, promo_start, promo_end, added_by, updated_by) " +
-                "VALUES (@PromoCode, @PromoName, @PromoDescription, @PromoStart, @PromoEnd, @AddedBy, @UpdatedBy)";
-            string constr = _configuration.GetConnectionString("SampleDbConnection");
-            using (NpgsqlConnection con = new NpgsqlConnection(constr)) 
-            {
-                using (NpgsqlCommand cmd = new NpgsqlCommand(query, con))
-                {
-                    cmd.Parameters.AddWithValue("PromoCode", data["promoCode"].ToString());
-                    cmd.Parameters.AddWithValue("PromoName", data["promoName"].ToString());
-                    cmd.Parameters.AddWithValue("PromoDescription", data["promoDescription"].ToString());
-                    cmd.Parameters.AddWithValue("PromoStart", DateTime.ParseExact(data["promoStart"].ToString(), "yyyy-mm-dd", System.Globalization.CultureInfo.InvariantCulture));
-                    cmd.Parameters.AddWithValue("PromoEnd", DateTime.ParseExact(data["promoEnd"].ToString(), "yyyy-mm-dd", System.Globalization.CultureInfo.InvariantCulture));
-                    cmd.Parameters.AddWithValue("AddedBy", data["addedBy"].ToString());
-                    cmd.Parameters.AddWithValue("UpdatedBy", data["updatedBy"].ToString());
-                    cmd.Connection = con;
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                    con.Close();
-                }
-            }
+            var returnMsg = new JObject();
 
-            return "Success";
+            string constr = _configuration.GetConnectionString("SampleDbConnection");
+            string query = "insert_promo";
+            var con = new NpgsqlConnection(constr);
+            var cmd = new NpgsqlCommand(query, con);
+
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("p_prm_code", data["promoCode"].ToString());
+            cmd.Parameters.AddWithValue("p_prm_name", data["promoName"].ToString());
+            cmd.Parameters.AddWithValue("p_prm_description", data["promoDescription"].ToString());
+            cmd.Parameters.AddWithValue("p_prm_start", DateOnly.ParseExact(data["promoStart"].ToString(), "yyyy-mm-dd", System.Globalization.CultureInfo.InvariantCulture));
+            cmd.Parameters.AddWithValue("p_prm_end", DateOnly.ParseExact(data["promoEnd"].ToString(), "yyyy-mm-dd", System.Globalization.CultureInfo.InvariantCulture));
+            cmd.Parameters.AddWithValue("p_added_by", data["addedBy"].ToString());
+            cmd.Parameters.AddWithValue("p_updated_by", data["updatedBy"].ToString());
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            returnMsg["statusCode"] = 201;
+            returnMsg["message"] = "Data updated successfully";
+            return returnMsg;
 
         }
 
         [HttpPut("{id}")]
-        public string update([FromBody] JsonObject data, int id)
+        public JObject Update([FromBody] JObject data, int id)
         {
-            string query = "UPDATE promos SET promo_code = @promoCode, promo_name = @PromoName, promo_description = @PromoDescription, promo_start = @PromoStart, promo_end = @PromoEnd, updated_by = @UpdatedBy " +
-                "WHERE id = @PromoId";
-            string constr = _configuration.GetConnectionString("SampleDbConnection");
-            using (NpgsqlConnection con = new NpgsqlConnection(constr))
-            {
-                using (NpgsqlCommand cmd = new NpgsqlCommand(query, con))
-                {
-                    cmd.Parameters.AddWithValue("PromoId", id);
-                    cmd.Parameters.AddWithValue("promoCode", data["promoCode"].ToString());
-                    cmd.Parameters.AddWithValue("PromoName", data["promoName"].ToString());
-                    cmd.Parameters.AddWithValue("PromoDescription", data["promoDescription"].ToString());
-                    cmd.Parameters.AddWithValue("PromoStart", DateTime.ParseExact(data["promoStart"].ToString(), "yyyy-mm-dd", System.Globalization.CultureInfo.InvariantCulture));
-                    cmd.Parameters.AddWithValue("PromoEnd", DateTime.ParseExact(data["promoEnd"].ToString(), "yyyy-mm-dd", System.Globalization.CultureInfo.InvariantCulture));
-                    cmd.Parameters.AddWithValue("UpdatedBy", data["updatedBy"].ToString());
-                    cmd.Connection = con;
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                    con.Close();
-                }
-            }
+            var returnMsg = new JObject();
 
-            return "Success";
+            string constr = _configuration.GetConnectionString("SampleDbConnection");
+            string query = "update_promo_by_id";
+            var con = new NpgsqlConnection(constr);
+            var cmd = new NpgsqlCommand(query, con);
+
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("p_prm_id", id);
+            cmd.Parameters.AddWithValue("p_prm_code", data["promoCode"].ToString());
+            cmd.Parameters.AddWithValue("p_prm_name", data["promoName"].ToString());
+            cmd.Parameters.AddWithValue("p_prm_description", data["promoDescription"].ToString());
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            returnMsg["statusCode"] = 200;
+            returnMsg["message"] = "Data updated successfully";
+            return returnMsg;
 
         }
 
         [HttpDelete("{id}")]
-        public string delete(int id)
+        public JObject Delete(int id)
         {
-            string query = "DELETE FROM promos WHERE id = @PromoId";
-            string constr = _configuration.GetConnectionString("SampleDbConnection");
-            using (NpgsqlConnection con = new NpgsqlConnection(constr))
-            {
-                using (NpgsqlCommand cmd = new NpgsqlCommand(query, con))
-                {
-                    cmd.Parameters.AddWithValue("PromoId", id);
-                    cmd.Connection = con;
-                    con.Open();
-                    cmd.ExecuteNonQuery();
-                    con.Close();
-                }
-            }
+            var returnMsg = new JObject();
 
-            return "Success";
+            string constr = _configuration.GetConnectionString("SampleDbConnection");
+            string query = "delete_promo_by_id";
+            var con = new NpgsqlConnection(constr);
+            var cmd = new NpgsqlCommand(query, con);
+
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("p_prm_id", id);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            returnMsg["statusCode"] = 200;
+            returnMsg["message"] = "Data deleted successfully";
+            return returnMsg;
 
         }
 
@@ -167,3 +163,17 @@ namespace testt.Controllers
     }
 
 }
+
+/*
+ 
+{
+    "status_code": 200,
+    "message": "Success",
+    "data": {
+        ...data
+    }
+}
+
+newtonsoft json
+ 
+ */
--- testt/Program.cs
+++ testt/Program.cs
@@ -1,8 +1,5 @@
-using Microsoft.EntityFrameworkCore;
-using testt.Data;
-
 var builder = WebApplication.CreateBuilder(args);
-/*builder.Services.AddDbContext<DataContext>();*/
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Add services to the container.
 
 builder.Services.AddControllers();
